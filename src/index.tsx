2a4e9e145cc67007164b8fb13de6f1b519839e79
Ari Jaya <arijaya130@gmail.com>
2022-12-28 17:54:26 +0800

    finish todo

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./redux/store";
+import  './index.css';
 import App from './App';
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +13,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
 
